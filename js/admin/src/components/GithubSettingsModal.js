03c8243370ccc72655bc82afa1ef4ead126ed06f
Toby Zerner <toby.zerner@gmail.com>
2015-09-18 13:31:18 +0930

    Use SettingsModal component

--- js/admin/src/components/GithubSettingsModal.js
+++ js/admin/src/components/GithubSettingsModal.js
@@ -1,15 +1,6 @@
-import Modal from 'flarum/components/Modal';
-import Button from 'flarum/components/Button';
-import saveConfig from 'flarum/utils/saveConfig';
+import SettingsModal from 'flarum/components/SettingsModal';
 
-export default class GithubSettingsModal extends Modal {
-  constructor(...args) {
-    super(...args);
-
-    this.clientId = m.prop(app.config['github.client_id'] || '');
-    this.clientSecret = m.prop(app.config['github.client_secret'] || '');
-  }
-
+export default class GithubSettingsModal extends SettingsModal {
   className() {
     return 'GithubSettingsModal Modal--small';
   }
@@ -18,47 +9,17 @@ export default class GithubSettingsModal extends Modal {
     return 'GitHub Settings';
   }
 
-  content() {
-    return (
-      <div className="Modal-body">
-        <div className="Form">
-          <div className="Form-group">
-            <label>Client ID</label>
-            <input className="FormControl" value={this.clientId()} oninput={m.withAttr('value', this.clientId)}/>
-          </div>
+  form() {
+    return [
+      <div className="Form-group">
+        <label>Client ID</label>
+        <input className="FormControl" bidi={this.setting('github.client_id')}/>
+      </div>,
 
-          <div className="Form-group">
-            <label>Client Secret</label>
-            <input className="FormControl" value={this.clientSecret()} oninput={m.withAttr('value', this.clientSecret)}/>
-          </div>
-
-          <div className="Form-group">
-            <Button
-              type="submit"
-              className="Button Button--primary GithubSettingsModal-save"
-              loading={this.loading}>
-              Save Changes
-            </Button>
-          </div>
-        </div>
+      <div className="Form-group">
+        <label>Client Secret</label>
+        <input className="FormControl" bidi={this.setting('github.client_secret')}/>
       </div>
-    );
-  }
-
-  onsubmit(e) {
-    e.preventDefault();
-
-    this.loading = true;
-
-    saveConfig({
-      'github.client_id': this.clientId(),
-      'github.client_secret': this.clientSecret()
-    }).then(
-      () => this.hide(),
-      () => {
-        this.loading = false;
-        m.redraw();
-      }
-    );
+    ];
   }
 }
