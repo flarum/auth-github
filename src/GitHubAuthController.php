613a8a40860b448eb1c1893ef04b32e4ebf97708
Toby Zerner <toby.zerner@gmail.com>
2015-12-03 15:13:56 +1030

    Update API

--- src/GitHubAuthController.php
+++ src/GitHubAuthController.php
@@ -83,6 +83,6 @@ class GitHubAuthController implements ControllerInterface
         $username = preg_replace('/[^a-z0-9-_]/i', '', $owner->getNickname());
         $avatarUrl = array_get($owner->toArray(), 'avatar_url');
 
-        return $this->authenticate(compact('email'), compact('username', 'avatarUrl'));
+        return $this->authenticate($request, compact('email'), compact('username', 'avatarUrl'));
     }
 }
