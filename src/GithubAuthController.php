438fb1ee5421af9201d683571fbaaa18f5416af8
Matt Kilgore <tankerkiller125@gmail.com>
2020-01-07 18:48:02 -0500

    Change Zend to Laminas (#13)

--- src/GithubAuthController.php
+++ src/GithubAuthController.php
@@ -14,13 +14,13 @@ use Flarum\Forum\Auth\Registration;
 use Flarum\Forum\Auth\ResponseFactory;
 use Flarum\Http\UrlGenerator;
 use Flarum\Settings\SettingsRepositoryInterface;
+use Laminas\Diactoros\Response\RedirectResponse;
 use League\OAuth2\Client\Provider\Github;
 use League\OAuth2\Client\Provider\GithubResourceOwner;
 use League\OAuth2\Client\Token\AccessToken;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\RedirectResponse;
 
 class GithubAuthController implements RequestHandlerInterface
 {
